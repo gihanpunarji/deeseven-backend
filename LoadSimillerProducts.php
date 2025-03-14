a5d2df60870ed94e7560dedfbd014e2a16ba052e
Gihan <gihanpunarji@gmail.com>
2025-03-14 13:51:45 +0530

    v3

--- LoadSimillerProducts.php
+++ LoadSimillerProducts.php
@@ -7,25 +7,27 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if (empty($_GET['category'])) {
+if (empty($_GET['subCategory'])) {
     return;
 } else {
 
-    $category_name = $_GET['category'];
-    $category_id;
-
-    $category_table = Database::search("SELECT `category_id` FROM `category` 
-    WHERE `category_name` = '" . $category_name . "'");
-
-    if ($category_table->num_rows == 1) {
-        $row = $category_table->fetch_assoc();
-        $category_id = $row['category_id'];
-    }
-
+    $sub_category_name = $_GET['subCategory'];
+    $product_id = $_GET['id'];
+    $sub_category_id;
+    
     $response = ["status" => false, "message" => "fetching failed", "data" => null];
 
+    $sub_category_table = Database::search("SELECT `sub_category_id` FROM `sub_category` 
+    WHERE `sub_category_name` = '" . $sub_category_name . "'");
+
+    if ($sub_category_table->num_rows == 1) {
+        $row = $sub_category_table->fetch_assoc();
+        $sub_category_id = $row['sub_category_id'];
+    }
+
+
     $table = Database::search("SELECT * FROM `product` WHERE 
-    product.category_category_id = $category_id ORDER BY `date_added` DESC");
+    product.sub_category_sub_category_id = $sub_category_id AND product.product_id != $product_id ORDER BY `date_added` DESC");
     $table2 = Database::search("SELECT * FROM `product_images`");
 
     $product_images = [];
